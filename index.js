d0375f28b1dab5183c6c7b3a9e41e595037fcc7b
Fernanda Rios <rios.fernanda1231@gmail.com>
2022-10-25 17:09:24 -0400

    Dicee original

---
+++ index.js
@@ -0,0 +1,41 @@
+// alert("Working!");
+
+//Dice 1
+
+var randomNumber1= Math.random();
+
+randomNumber1 = Math.floor ((randomNumber1 * 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//Dice 2
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//Winner
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!😛";
+}
+
+
+
+
+
+
